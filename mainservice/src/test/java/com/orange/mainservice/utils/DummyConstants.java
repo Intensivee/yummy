6916fd45e3c1d56b0be183306758f0eb59c06505
intensivee <mrorange016@gmail.com>
2021-09-17 08:36:58 +0200

    CommentControllerIT.java cleanup

---
+++ mainservice/src/test/java/com/orange/mainservice/utils/DummyConstants.java
@@ -0,0 +1,12 @@
+package com.orange.mainservice.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class DummyConstants {
+
+    public static final int DUMMY_INT = 123456789;
+    public static final long DUMMY_LONG = 1234556789L;
+    public static final String F = "123456789";
+}
